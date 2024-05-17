788e7829200352688b126f3c8f09ca923b001566
Aditi Pathak <Aditi55Pathak>
2024-05-17 11:20:49 +0530

    my commit

---
+++ Array/prac6.py
@@ -0,0 +1,37 @@
+#  Search in Rotated Sorted Array
+
+
+# Method for sorting the array
+
+def bSearch(nums, target):
+    left, right = 0, len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+
+        if nums[left] <= nums[mid]:
+            if nums[left] <= target <= nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        else:
+            if nums[mid] <= target <= nums[right]:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+    return -1
+
+
+# Taking the user input for array and target value
+
+nums = input("Enter the array of numbers: ").split()
+nums = [int(num) for num in nums]
+target = int(input("Enter the target number: "))
+result = bSearch(nums, target)
+print("Element found at index", result)
+
+
+
